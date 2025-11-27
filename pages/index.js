92a8fcc91a6c33a0b88badaa4d5c37cae9118242
Marcus Perdigão <marcusperdigao99@gmail.com>
2025-11-27 16:31:15 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-function Home(){
-    return <h1>Mãe, eu te amo! Espero que você melhore e fique bem ♥ </h1>
+function Home() {
+  return <h1>Mãe, eu te amo! Espero que você melhore e fique bem ♥ </h1>;
 }
 
 export default Home;
-
